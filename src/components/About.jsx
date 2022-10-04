531ca23e71d7ece16e51a0b199366f6299511a70
Rei Sasajima <sasajimarei@gmail.com>
2022-10-04 16:16:27 +0900

    fisrt

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import '../static/About.css'
+
+export default function About() {
+  return (
+    <div className='AboutWrapper'>
+      <p>My Name is Rei Sasajima.<br></br>Im undergraduate in AoyamaGakuinUni.</p>
+    </div>
+      )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../static/Header.css'
+
+export default function Header() {
+  return (
+    <div className='HeaderWrapper'>
+        <a  className='name' href="https://twitter.com/r315513">Rei Sasajima</a>
+        <a href="https://twitter.com/r315513">Works</a>
+        <a href="https://twitter.com/r315513">Collection</a>
+      
+    </div>
+      )
+}
