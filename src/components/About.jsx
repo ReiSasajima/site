c15db8669985311e3c371a447e568d1f685697da
Rei Sasajima <sasajimarei@gmail.com>
2022-10-05 16:24:01 +0900

    add temporary profile to about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,9 @@ import '../static/About.css'
 export default function About() {
   return (
     <div className='AboutWrapper'>
-      <p>My Name is Rei Sasajima.<br></br>Im undergraduate in AoyamaGakuinUni.</p>
+      <h1>Rei Sasajima</h1>
+      <p>Im undergraduate in AoyamaGakuinUni.<br></br>Expected Gradiation in Bachelor of Law</p>
+      <h3>Python・Flask・HTML・CSS・JavaScript・Ajax・SQLite<br></br>Adobe AfterEffects・PremierePro・Illustrator・Photoshop・Figma</h3>
     </div>
       )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,9 @@ import '../static/Header.css'
 export default function Header() {
   return (
     <div className='HeaderWrapper'>
-        <a  className='name' href="https://twitter.com/r315513">Rei Sasajima</a>
-        <a href="https://twitter.com/r315513">Works</a>
-        <a href="https://twitter.com/r315513">Collection</a>
-      
+        <a  className='name' href="https://twitter.com/r315513" rel="noreferrer" target="_blank">Rei Sasajima</a>
+        <a className='works' href="https://tadable.herokuapp.com/" rel="noreferrer" target="_blank">Works</a>
+        <a className='collection' href="https://twitter.com/r315513" rel="noreferrer" target="_blank">Collection</a>
     </div>
       )
 }
