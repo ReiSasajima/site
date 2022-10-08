ef1f183e532aa4f14b113994167d9e524daabb8e
Rei Sasajima <sasajimarei@gmail.com>
2022-10-08 19:56:48 +0900

    add baccarat-ranking for works list page.

--- src/components/WorksList.jsx
+++ src/components/WorksList.jsx
@@ -3,9 +3,17 @@ import Tadable from '../static/img/TadableThumbnail.jpg'
 import ifailed from '../static/img/ifailed.png'
 import agusyllabus from '../static/img/agusyllabus.jpeg'
 import senryu from '../static/img/senryu.png'
+import baccarat from '../static/img/baccarat.jpeg'
 const WorksList = () => {
   return (
     <div className='List-Wrapper'>
+      <a className='List' href="https://agusyllabus.deta.dev/">
+        <div>
+          <img src={baccarat} alt="作品の画像" />
+          <h3 className='List-Title'>Baccarat Ranking</h3>
+          <p className='List-Description'>Ranking system for school festival</p>
+        </div>
+      </a>
       <a className='List' href="https://tadable.herokuapp.com/">
         <div>
           <img src={Tadable} alt="作品の画像" />
@@ -27,7 +35,7 @@ const WorksList = () => {
           <p className='List-Description'>Recreated syllabus searching system of University</p>
         </div>
       </a>
-      <a className='List' href="https://agusyllabus.deta.dev/">
+      <a className='List' href="https://casinorank.herokuapp.com/">
         <div>
           <img src={senryu} alt="作品の画像" />
           <h3 className='List-Title'>アニメ風川柳・俳句メーカー</h3>
