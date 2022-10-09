6a1e686251bcc76f9e7028a5e6471604b950c87a
Rei Sasajima <sasajimarei@gmail.com>
2022-10-09 23:33:57 +0900

    fixed typo and add more margin on socialmedia comoponents in smartphone mode

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
       <div className='AboutWrapper'>
         <h1 className='AboutWrapper-name'>Rei Sasajima</h1>
-        <p className='AboutWrapper-description'>Im undergraduate in AoyamaGakuinUniversity.<br></br>Expected Gradiation in Bachelor of Law</p>
+        <p className='AboutWrapper-description'>Im undergraduate in AoyamaGakuinUniversity.<br></br>Expected Graduation in Bachelor of Law</p>
       </div>
       )
 }
