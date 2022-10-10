feed7d0a816493a3c2aa94cdeebbf411bee88d45
Rei Sasajima <sasajimarei@gmail.com>
2022-10-11 06:02:07 +0900

    created components for Work content

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,14 +1,36 @@
 import NavBar from "./NavBar"
 import '../static/Works.css'
 import WorksList from "./WorksList"
-import AnimatedPage from "./AnimatedPage"
+import tadable from '../static/img/TadableThumbnail.jpg'
+import baccarat from '../static/img/baccarat.jpeg'
+import ifailed from '../static/img/ifailed.png'
+import agusyllabus from '../static/img/agusyllabus.jpeg'
+import senryu from '../static/img/senryu.png'
+import rinri from '../static/img/RinriHatanThumbnail.png'
+import one from '../static/img/ONE.jpg'
+
 
 const Works = () => {
   return (
     <>
       <NavBar/>
       <h1 className="section-title">Works</h1>
-      <WorksList/>
+      <div className="List-Wrapper">
+        <WorksList img={tadable} name={'Tadable'} text={'無料で見れる漫画'}
+          src={'https://tadable.herokuapp.com/'} />
+        <WorksList img={baccarat} name={'Baccarat Ranking'} text={'Ranking system for school festival'}
+          src={'https://casinorank.herokuapp.com/'} />
+        <WorksList img={ifailed} name={'落単報告メーカー'} text={'Image creating service for Twitter when you failed class'}
+          src={'https://ifailed-a08cd.web.app/'} />
+        <WorksList img={agusyllabus} name={'AGU SyllabusSearcher Remix'} text={"Recreated university's syllabus searching system"} src={'https://agusyllabus.deta.dev/'}/>
+        <WorksList img={senryu} name={'アニメ風川柳・俳句メーカー'} text={"Image creating service you can create traditional literary creation look like japanese anime title"}
+          src={'https://manga-80f94.web.app/'} />
+        <WorksList img={rinri} name={'ショートドラマ『倫理破綻』'} text={"Story about four terrible university students"}
+          src={'https://youtu.be/ZWRIBzV1HWU'} />
+        <WorksList img={one} name={'メイキング映像『Creating One』'} text={"Behind the scenes of Music Video 'One' "}
+          src={'https://www.youtube.com/watch?v=bnoYtpCUaJM'} />
+
+      </div>
     </>
   )
 }
--- src/components/WorksList.jsx
+++ src/components/WorksList.jsx
@@ -1,49 +1,14 @@
 import '../static/List.css'
-import Tadable from '../static/img/TadableThumbnail.jpg'
-import ifailed from '../static/img/ifailed.png'
-import agusyllabus from '../static/img/agusyllabus.jpeg'
-import senryu from '../static/img/senryu.png'
-import baccarat from '../static/img/baccarat.jpeg'
-const WorksList = () => {
+
+const WorksList = (props) => {
   return (
-    <div className='List-Wrapper'>
-      <a className='List' href="https://agusyllabus.deta.dev/">
+      <a className='List' href={props.src} target="_blank" rel="noreferrer noopener">
         <div>
-          <img src={baccarat} alt="作品の画像" />
-          <h3 className='List-Title'>Baccarat Ranking</h3>
-          <p className='List-Description'>Ranking system for school festival</p>
+        <img src={props.img} alt="作品の画像" />
+        <h3 className='List-Title'>{ props.name}</h3>
+        <p className='List-Description'>{props.text}</p>
         </div>
       </a>
-      <a className='List' href="https://tadable.herokuapp.com/">
-        <div>
-          <img src={Tadable} alt="作品の画像" />
-          <h3 className='List-Title'>Tadable</h3>
-          <p className='List-Description'>Service you can find where to read free comics</p>
-        </div>
-      </a>
-      <a className='List' href="https://ifailed-a08cd.web.app/">
-        <div>
-          <img src={ifailed} alt="作品の画像" />
-          <h3 className='List-Title'>落単報告メーカー</h3>
-          <p className='List-Description'>Image creating service for Twitter when you failed class</p>
-        </div>
-      </a>
-      <a className='List' href="https://agusyllabus.deta.dev/">
-        <div>
-          <img src={agusyllabus} alt="作品の画像" />
-          <h3 className='List-Title'>AGU SyllabusSearcher Remix</h3>
-          <p className='List-Description'>Recreated syllabus searching system of University</p>
-        </div>
-      </a>
-      <a className='List' href="https://casinorank.herokuapp.com/">
-        <div>
-          <img src={senryu} alt="作品の画像" />
-          <h3 className='List-Title'>アニメ風川柳・俳句メーカー</h3>
-          <p className='List-Description'>You can create literary creation like japanese anime's title</p>
-        </div>
-      </a>
-      
-    </div>
   )
 }
 export default WorksList
