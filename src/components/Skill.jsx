6b7f9b383020c21a8f176b53ea62feff5979823b
Rei Sasajima <sasajimarei@gmail.com>
2022-10-13 02:43:17 +0900

    fullfilled paragraph in Skill.jsx

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -4,7 +4,7 @@ const Skill=()=> {
   return (
     <div className='Skill-Wrapper'>
       <h1 className='Skill-Wrapper-Title'>My skill sets</h1>
-      <p className='Skill-Wrapper-Text'>Python・Flask・HTML・CSS・JavaScript・Ajax・SQLite</p>
+      <p className='Skill-Wrapper-Text'>Python・Flask・HTML・CSS・JavaScript・Ajax・SQL・C</p>
       <p className='Skill-Wrapper-Text'>Blender・Adobe AfterEffects・PremierePro・Illustrator・Photoshop・Figma</p>
     </div>
       )
