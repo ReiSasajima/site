41ef57d4f81921177e5d2b273099ab15be8d43b3
Rei Sasajima <sasajimarei@gmail.com>
2022-10-14 16:34:08 +0900

    add cs50 certificates description

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -6,6 +6,7 @@ const Skill=()=> {
       <h1 className='Skill-Wrapper-Title'>My skill sets</h1>
       <p className='Skill-Wrapper-Text'>Python・Flask・HTML・CSS・JavaScript・Ajax・SQL・C</p>
       <p className='Skill-Wrapper-Text'>Blender・Adobe AfterEffects・PremierePro・Illustrator・Photoshop・Figma</p>
+      <a className='Skill-Wrapper-CS50' href="https://certificates.cs50.io/3781223a-5e88-4157-ab17-cb5d71fd706c.pdf?size=letter" target="_blank" rel="noreferrer noopener"><h3>Completed Harvard University CS50X</h3></a>
     </div>
       )
 }
